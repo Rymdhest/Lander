bf8fed84771d7bc320805c3f8b1edccfbe946040
Gustav Holmström <gustav.holmstroem@gmail.com>
2023-02-23 20:15:21 +0100

    lagt till danmark, italien, tjeckien och rumänien.

--- D14-Ovn1/Program.cs
+++ D14-Ovn1/Program.cs
@@ -22,5 +22,15 @@ internal class Program
         Country sweden = new Country() { country = "Sweden" , rule="Monarch", capital = "Stockholm", population= 10512820 };
         Country germany = new Country() { country = "Germany", rule = "Republic", capital = "Berlin", population = 83783902 };
         Country sanMarino = new Country() { country = "San Marino", rule = "Republic", capital = "San Marino", population = 33600 };
+
+        Country[] countries = new Country[7];
+        countries[0] = sweden;
+        countries[1] = germany;
+        countries[2] = sanMarino;
+
+        countries[3] = new Country() { country = "Denmark", rule = "Monarch", capital = "Stockholm", population = 5928364 };
+        countries[3] = new Country() { country = "Italy", rule = "Republic", capital = "Rom", population = 58853482 };
+        countries[3] = new Country() { country = "Czech", rule = "Republic", capital = "Prague", population = 10551219 };
+        countries[3] = new Country() { country = "Romania", rule = "Republic", capital = "Bukarest", population = 19760314 };
     }
 }
