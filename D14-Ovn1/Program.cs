d3bc0d16d8b3d29c3b3f6ab59ca325af19950c78
Gustav Holmström <gustav.holmstroem@gmail.com>
2023-02-23 19:43:48 +0100

    Add project files.

---
+++ D14-Ovn1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal class Program
+{
+    class Person
+    {
+        public string country;
+        public string rule;
+        public string capital;
+        public int population;
+
+        public void print()
+        {
+            System.Console.WriteLine($"Country: {country}");
+            System.Console.WriteLine($"Rule: {rule}");
+            System.Console.WriteLine($"capital: {capital}");
+            System.Console.WriteLine($"Population: {population} \n");
+        }
+    }
+    private static void Main(string[] args)
+    {
+    
+    }
+}
